fa4b153a20b764e4b526098e1b82176bc9c960c1
Nonoka Homma <e185730@ie.u-ryukyu.ac.jp>
2019-01-21 21:56:23 +0900

    JavaDoc形式でドキュメントを書いた

--- src/main/java/jp/ac/uryukyu/ie/e185730/MakeCards.java
+++ src/main/java/jp/ac/uryukyu/ie/e185730/MakeCards.java
@@ -10,7 +10,6 @@ public class MakeCards {
      * @return 作ったリスト
      */
     public List<Integer> makeCards(){
-        //List<Integer> cardslist = new ArrayList<Integer>();
         for (int i=0;i<20;i++){
             cardslist.add((int)(Math.random()*100));
         }
